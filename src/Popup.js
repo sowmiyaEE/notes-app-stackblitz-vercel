a10408a8650516eeb8e71d28dd6a9c85a8b50855
sowmiyaEE <sowmiya.skm@gmail.com>
2024-12-03 21:43:21 +0530

    Update Popup.js

--- src/Popup.js
+++ src/Popup.js
@@ -167,7 +167,7 @@ export function Popcontent({
     notetagline = useRef(),
     notedetail = useRef(),
     noteid = useRef(),
-    notedate = useRef(),
+    notedate = useRef();
 
   const onKeyPressed = (event) => {
     if (event.keyCode == 13) {
